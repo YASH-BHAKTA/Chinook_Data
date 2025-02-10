eea2e4704555556acb93b1ee46a149874a38fb70
YASH-BHAKTA <ybhakta1000@gmail.com>
2025-02-10 10:55:01 -0400

    changes made

--- app.js
+++ app.js
@@ -7,23 +7,23 @@ const app = express()
 
 //Configure how multer behaves
 const storage = multer.diskStorage({
-    destination:'./uploads', 
-    filename: function(req, file, callback){
+    destination: './uploads',
+    filename: function (req, file, callback) {
         const newName = `upload_${file.originalname}`
         callback(null, newName);
     }
 })
 
-const upload = multer({storage: storage})
+const upload = multer({ storage: storage })
 
 //Establish a connection to our database
-export const db = new Database('./database/chinook.sqlite', {fileMustExist: true})
+export const db = new Database('./database/chinook.sqlite', { fileMustExist: true })
 // app.set('db', db);
 
 //Middleware
 app.use(express.json()) //Capture any payload in json format and assign to req.body
 app.use(express.static("public"));
-app.use(express.urlencoded({extended: true})) //Capture any payload in x-www-form-urlencoded and assign to req.body
+app.use(express.urlencoded({ extended: true })) //Capture any payload in x-www-form-urlencoded and assign to req.body
 app.use('/employees', employeesRouter) //Send all employees requests to the employees router
 
 //Endpoint!!!
@@ -39,7 +39,7 @@ app.post('/', (req, res) => {
 
 app.post('/api/employees/upload', upload.single('image'), (req, res) => {
     // console.log(req)
-    res.json({message: 'Done'})
+    res.json({ message: 'Done' })
 })
 
 //app.put => Replace the entire record's data with a new record
@@ -49,3 +49,5 @@ app.post('/api/employees/upload', upload.single('image'), (req, res) => {
 app.listen(3000, () => {
     console.log(`Listening on port 3000`)
 })
+
+//some changes made for testing
